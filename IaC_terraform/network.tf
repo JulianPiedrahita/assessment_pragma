8c65f6f2502901ab409666c86db245d92182855c
JulianPiedrahita <65320911+JulianPiedrahita@users.noreply.github.com>
2023-12-19 11:11:28 -0500

    Update network.tf

--- IaC_terraform/network.tf
+++ IaC_terraform/network.tf
@@ -1,33 +1,19 @@
-resource "aws_vpc" "web" {
-  cidr_block = var.vpc_web
-  tags = {
-    Name = "web-vpc"
-  }
-}
+module "network" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
 
-resource "aws_subnet" "web_sub_1" {
-  vpc_id            = aws_vpc.web.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "web-subnet-1"
-  }
-}
+  name = "web"
+  cidr = "10.0.0.0/16"
 
-resource "aws_subnet" "web_sub_2" {
-  vpc_id            = aws_vpc.web.id
-  cidr_block        = "10.0.20.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "web-subnet-2"
-  }
-}
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-resource "aws_subnet" "web_sub_3" {
-  vpc_id            = aws_vpc.web.id
-  cidr_block        = "10.0.30.0/24"
-  availability_zone = "us-east-1c"
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
   tags = {
-    Name = "web-subnet-3"
+    Terraform   = "true"
+    Environment = "youtube"
   }
 }

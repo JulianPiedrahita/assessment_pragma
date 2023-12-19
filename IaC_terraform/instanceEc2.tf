991b9819c841d7ffa035fd63c43c1df19a5029b2
JulianPiedrahita <65320911+JulianPiedrahita@users.noreply.github.com>
2023-12-19 11:12:07 -0500

    Update instanceEc2.tf

--- IaC_terraform/instanceEc2.tf
+++ IaC_terraform/instanceEc2.tf
@@ -1,11 +1,25 @@
-resource "aws_instance" "web_server" {
-  ami                        = "ami-4567890"
-  instance_type              = "t3.nano"
-  subnet_id                  = aws_subnet.web_sub_1.id
-  vpc_security_group_ids     = [aws_security_group.server.id]
-  associate_public_ip_adress = var.include_ipv4
+resource "aws_instance" "public_server" {
+  count                  = var.public_server_count
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = aws_key_pair.server.key_name
+  instance_type          = var.server_type
+  subnet_id              = module.network.public_subnets[count.index]
+  vpc_security_group_ids = [aws_security_group.server.id]
+
+  associate_public_ip_address = var.include_ipv4
   tags = {
-    Name = "Hola mundo"
+    Name = "PublicServer-${count.index}"
   }
 }
+
+resource "aws_key_pair" "server" {
+  key_name   = "youtube"
+  public_key = tls_private_key.server.public_key_openssh
+}
+
+resource "tls_private_key" "server" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 
